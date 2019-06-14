cddeb682cacea4f8c9200b342cc66a582419d429
Singh-Sg <singhsg@example.com>
2019-06-14 19:43:51 +0530

    Make InfraVltgDetail model and view

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import *
+
+def index(request):
+
+    data = InfraVltgDetail.objects.all()
+    import pdb; pdb.set_trace()
+    context = {'data': data.count()}
+    return render(request, 'portal/index.html', context)
