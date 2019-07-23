19cbbf7d182509ea6f63e315d2848bf8249ccfa7
Singh-Sg <singhsg@example.com>
2019-07-23 18:56:38 +0530

    Update ORM to SqlRow query

--- portal/views.py
+++ portal/views.py
@@ -31,21 +31,21 @@ def index(request):
     cur = conn.cursor()
 
     #by environment
-    cur.execute('SELECT "portal_infravltgdetail"."environment", COUNT("portal_infravltgdetail"."environment") AS "name_count" FROM "portal_infravltgdetail" GROUP BY "portal_infravltgdetail"."environment"')
+    # cur.execute('SELECT "portal_infravltgdetail"."environment", COUNT("portal_infravltgdetail"."environment") AS "name_count" FROM "portal_infravltgdetail" GROUP BY "portal_infravltgdetail"."environment"')
+    cur.execute('SELECT environment, COUNT(environment) FROM portal_infravltgdetail GROUP BY environment')
+    
     environment = cur.fetchall()
 
     #by user
-    cur.execute('SELECT "portal_infravltgdetail"."username", COUNT("portal_infravltgdetail"."username") AS "name_count" FROM "portal_infravltgdetail" GROUP BY "portal_infravltgdetail"."username"')
+    # cur.execute('SELECT "portal_infravltgdetail"."username", COUNT("portal_infravltgdetail"."username") AS "name_count" FROM "portal_infravltgdetail" GROUP BY "portal_infravltgdetail"."username"')
+    cur.execute('SELECT username, COUNT(username) AS name_count FROM portal_infravltgdetail GROUP BY username')
     byuser = cur.fetchall()
     
     # by time
-    by_month = InfraVltgDetail.objects.annotate(month=TruncMonth('create_dt')).values('month').annotate(name_count=Count('id')).values('month', 'name_count')
-    # import pdb; pdb.set_trace()
+    # by_month = InfraVltgDetail.objects.annotate(month=TruncMonth('create_dt')).values('month').annotate(name_count=Count('id')).values('month', 'name_count')   
+    cur.execute("SELECT create_dt, COUNT(create_dt)  FROM portal_infravltgdetail GROUP BY create_dt")
+    by_month = cur.fetchall()
 
-    # dddd = cur.execute(by_month.query)
-
-    # cur.execute('SELECT TRUNC("month", "portal_infravltgdetail"."create_dt", UTC) AS month, COUNT("portal_infravltgdetail"."id") AS "name_count" FROM "portal_infravltgdetail" GROUP BY django_datetime_trunc("month", "portal_infravltgdetail"."create_dt", UTC)')
-    # by_month = cur.fetchall()
     cur.execute('SELECT COUNT(*) from portal_infravltgdetail')
     data = cur.fetchall()
     context = {'data': data, 'environment': environment,
@@ -59,7 +59,10 @@ def envByUserName(request, *args, **kwargs):
     """
     try:
         env = request.GET['environment']
-        username = InfraVltgDetail.objects.filter(environment=env).values("username").annotate(name_count=Count('username'))
+        # username = InfraVltgDetail.objects.filter(environment=env).values("username").annotate(name_count=Count('username'))
+        cur = conn.cursor()
+        cur.execute("SELECT username,  COUNT(username)  FROM portal_infravltgdetail where environment= '"+env+"' GROUP BY username")
+        username = cur.fetchall()
         users = []
         for user in username:
             users.append(user)
@@ -74,9 +77,13 @@ def envByMonth(request, *args, **kwargs):
     try:
         env = request.GET['environment']
         username = request.GET['username']
-        query_object =Q(environment=env) & Q(username=username)
-        by_month = InfraVltgDetail.objects.filter(query_object).annotate(month=TruncMonth('create_dt')).values('month').annotate(name_count=Count('id')).values('month', 'name_count')
-        by_month_data = json.dumps(list(by_month), cls = DjangoJSONEncoder)
+        # query_object =Q(environment=env) & Q(username=username)
+        # by_month = InfraVltgDetail.objects.filter(query_object).annotate(month=TruncMonth('create_dt')).values('month').annotate(name_count=Count('id')).values('month', 'name_count')
+        # by_month_data = json.dumps(list(by_month), cls = DjangoJSONEncoder)
+        cur = conn.cursor()
+        cur.execute("SELECT create_dt,  COUNT(create_dt)  FROM portal_infravltgdetail where (environment= '"+env+"') AND (username= '"+username+"') GROUP BY create_dt")
+        username1 = cur.fetchall()
+        by_month_data = json.dumps(list(username1), cls = DjangoJSONEncoder)
         return JsonResponse({"data": by_month_data, 'status_code': "200"})
     except Exception:
         return JsonResponse({'error': 'invalid request', 'status': 'fail'})
@@ -87,7 +94,11 @@ def environmentsByUser(request, *args, **kwargs):
     """
     try:
         username = request.GET['username']
-        environments = InfraVltgDetail.objects.filter(username=username).values("environment").annotate(name_count=Count('environment'))
+        # environments = InfraVltgDetail.objects.filter(username=username).values("environment").annotate(name_count=Count('environment'))
+        cur = conn.cursor()
+        # import pdb; pdb.set_trace()
+        cur.execute("SELECT environment,  COUNT(environment)  FROM portal_infravltgdetail where username= '"+username+"' GROUP BY environment")
+        environments = cur.fetchall()
         env = []
         for environment in environments:
             env.append(environment)
@@ -101,8 +112,14 @@ def environmentsByMonth(request, *args, **kwargs):
     """
     try:
         month = request.GET['month']
+        year = request.GET['year']
         environments = InfraVltgDetail.objects.filter(create_dt__month=month).values("environment").annotate(name_count=Count('environment'))
+        import pdb; pdb.set_trace()
+        
         env = []
+        cur = conn.cursor()
+        cur.execute('SELECT environment, COUNT(environment)FROM portal_infravltgdetail WHERE WHERE MONTH(create_dt) = 6 AND YEAR(create_dt) = 2019 GROUP BY environment')
+        environments = cur.fetchall()
         for environment in environments:
             env.append(environment)
         return JsonResponse({"data": env, 'status_code': "200", 'month': month})
